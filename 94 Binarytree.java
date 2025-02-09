962a084bbc3506f63c1713cca63c940755fc3b1b
rupesh <rupeshsai67@gmail.com>
2025-02-10 00:01:41 +0530

    94question

---
+++ 94 Binarytree.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderHelper(root, result);
+        return result;
+    }
+
+    private void inorderHelper(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorderHelper(root.left, result);
+        result.add(root.val);
+        inorderHelper(root.right, result);
+    }
+}
