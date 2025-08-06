1863431d233771ecdc02b26454e38a11106a04b5
sairupesh2005 <155470417+sairupesh2005@users.noreply.github.com>
2025-08-06 20:54:44 +0530

    problem

--- q1.java
+++ q1.java
@@ -1,6 +1,12 @@
-public class q1 {
-    public static void main(String[] args) {
-        System.out.println("hellow world");
-        
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
     }
-}
+} 
